5e1bc0ed9c04157fda17a56ab46896affcf0f12d
Sonny Sangha <sonny.sangha@gmail.com>
2024-04-23 14:27:24 +0400

    updates

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## For FREE Mentorship, Coaching Calls & Exclusive Content / Lessons, join Sonny's FREE community - [`University of Code (universityocode.com)`](https://universityofcode.com)
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
 import { Toaster } from "@/components/ui/sonner";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LinkedIn 2.0 clone",
